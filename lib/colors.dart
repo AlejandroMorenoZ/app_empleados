355bf9d1e7d7d46daf9f4c04e984e4732a709f27
Alejandro Alberto Moreno Zatarain <alalmorenoza@ittepic.edu.mx>
2019-07-24 09:07:52 -0700

    Initial commit

---
+++ lib/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const kShrineBlue50 = const Color(0xFFE0F2F1);
+const kShrinePink100 = const Color(0xFFE0F2F1);
+const kShrinePink300 = const Color(0xFFE0F2F1);
+const kShrinePink400 = const Color(0xFF00695C);
+
+
+const kShrineBrown900 = const Color(0xFF000000);
+
+const kShrineErrorRed = const Color(0xFFC5032B);
+
+const kShrineSurfaceWhite = const Color(0xFFFFFBFA);
+const kShrineBackgroundWhite = Colors.white;
